8a1c23b5bdd89ab6ce6f934a416869b18cbefef4
gaurverma <55631123+gaurverma@users.noreply.github.com>
2021-07-09 22:07:09 +0530

    Add files via upload

---
+++ Number of provinces.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int n;
+    void DFS(vector<vector<int>>& isConnected,vector <bool>& visited,int s){
+          visited[s] = true;
+          for(int j=0;j<n;j++){
+              if(visited[j]==false && isConnected[s][j])
+                  DFS(isConnected,visited,j);
+          }
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        n = isConnected.size();
+        vector <bool> visited(n,false);
+        int  count = 0;
+        for(int i=0;i<n;i++){
+            if(visited[i]==false){
+                DFS(isConnected,visited,i);
+                count++;
+            }
+        }
+        return count;
+    }
+};
